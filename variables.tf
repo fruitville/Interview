d147a7953538ba85ea0d7a7f908bee85931d1d12
fruitville <125894356+fruitville@users.noreply.github.com>
2023-12-26 12:12:30 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,13 +11,13 @@ variable "vpc_cidr_block" {
 variable "public_subnet_cidr_blocks" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24"]
 }
 
 variable "private_subnet_cidr_blocks" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.2.0/24"]
 }
 
 variable "key_name" {
